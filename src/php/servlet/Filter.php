801de9b6762d8aae4eab6a5672d0884a99ba598f
Tony Yip <tonyhhyip@gmail.com>
2014-12-18 17:31:47 +0800

    new file:   src/php/servlet/Filter.php

---
+++ src/php/servlet/Filter.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @package \php\servlet
+ * package php.servlet;
+ * @file Filter
+ */
+namespace \php\servlet;
+
+/**
+ * A filter is an object that performs filtering tasks on either the request to
+ * a resource (a servlet or static container), or on the response from a resource,
+ * or both.
+ * 
+ * Filters perform filtering in the <code>doFilter</code> method. Every Filter has access to 
+ * a FilterConfig object from which it can obtain its initialization parameters, a reference
+ * to the ServletContext which it can use, for example, to load resources needed for
+ * filtering tasks.
+ * 
+ * Filter can configured in the deployment descriptor of a web application.
+ * 
+ */
+
+?>
