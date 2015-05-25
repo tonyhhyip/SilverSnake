10f4403c0b27c24ad4f54e3ccd27178af72c6452
Tony Yip <tony@opensource.hk>
2015-05-25 22:13:44 +0800

    Manage Database by id

--- lib/Database/Database.php
+++ lib/Database/Database.php
@@ -17,6 +17,12 @@ abstract class Database
      */
     private static $dataSource = array();
 
+    /**
+     * @var array
+     */
+    private static $connection = array();
+
+
     public static function loadDataSource()
     {
         $content = file_get_contents(__DIR__ . '/../../config/dataSource.json', true);
@@ -27,23 +33,55 @@ abstract class Database
         self::$state = true;
     }
 
+    /**
+     * @param string $id
+     *
+     * @return \Doctrine\DBAL\Connection
+     * @throws \Doctrine\DBAL\DBALException
+     */
     public static function getConnection($id)
     {
-        return DriverManager::getConnection(self::$dataSource[$id]);
+        if (!array_key_exists($id, self::$connection))
+            self::$connection[$id] = DriverManager::getConnection(self::$dataSource[$id]);
+        return self::$connection[$id];
     }
 
+    /**
+     * @param string $id
+     *
+     * @return \Doctrine\DBAL\Query\QueryBuilder
+     */
+    public static function getQueryBuilder($id)
+    {
+        $conn = self::getConnection($id);
+        return $conn->createQueryBuilder();
+    }
+
+    /**
+     * @param string $id
+     * @param array $param
+     */
     public static function addDataSource($id, array $param)
     {
         self::$dataSource[$id] = $param;
     }
 
+    /**
+     * @param string $id
+     * @param array $param
+     */
     public static function updateDataSource($id, array $param)
     {
         self::$dataSource[$id] = $param;
+        unset(self::$connection[$id]);
     }
 
+    /**
+     * @param string $id
+     */
     public static function removeDataSource($id)
     {
         unset(self::$dataSource[$id]);
+        unset(self::$connection[$id]);
     }
 }
